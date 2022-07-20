45cbeb45dfab6e4720d7a0b8d419dfa89dd82cc2
Nestor Sanchez <nesaenz.69@gmail.com>
2022-07-20 16:51:08 -0500

    Se realizo el formularioc con Formik con estilos Tailwindcss

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -29,7 +29,9 @@ export const Layout = () => {
           </Link>
         </nav>
       </div>
-      <div className="md:w-3/4 p-10">{<Outlet />}</div>
+      <div className="md:w-3/4 p-10 md:h-screen overflow-scroll">
+        {<Outlet />}
+      </div>
     </div>
   );
 };
